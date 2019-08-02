8554582a7cc4de7292cff3dc7fad0c4ec9895311
arya <aryad999dev@gmail.com>
2019-08-02 13:41:15 -0700

    .env to gitignore

--- config/development.js
+++ config/development.js
@@ -1,3 +1,8 @@
 module.exports = {
-
+    mysql: {
+        host: 'localhost',
+        user: 'root',
+        password: process.env.mysql_root_pass,
+        database: process.env.mysql_database
+    }
 }
