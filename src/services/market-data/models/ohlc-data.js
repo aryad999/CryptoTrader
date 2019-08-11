974b2d2c62ec2c433733d06d14fbb4921473f002
arya <aryad999dev@gmail.com>
2019-08-10 20:16:15 -0700

    added more folders and files to define general project structure and services

--- src/services/market-data/models/ohlc-data.js
+++ src/services/market-data/models/ohlc-data.js
@@ -2,11 +2,22 @@ const Connection = require('../../../models/connection');
 
 function insertOHLC(timeCode, params) {
     const query =
-        " INSERT INTO ohlc_" + timeCode +" (currency_pair, time, endtime, open, high, low, close, vwap, volume, count ) " +
+        " INSERT INTO ohlc_" + timeCode +
+        " (currency_pair, time, endtime, open, high, low, close, vwap, volume, count ) " +
         " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?); ";
 
     return Connection.query(query, params);
 }
 
+function getOHLCByTimestamp(timeCode, timestampFrom) {
+    const query =
+        " SELECT time, open, high, low, close, volume " +
+        " FROM ohlc_" + timeCode +
+
+        " WHERE time > ?; "
+
+    const params = [timestampFrom];
+    return Connection.query(query, params);
+}
 
 module.exports.insertOHLC = insertOHLC;
