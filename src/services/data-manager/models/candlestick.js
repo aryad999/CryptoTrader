248d16f54c76cd0c7630cf322ec2fd3c9b70a9ab
arya <aryad999dev@gmail.com>
2019-08-11 15:01:21 -0700

    created candlestick class

--- src/services/data-manager/models/candlestick.js
+++ src/services/data-manager/models/candlestick.js
@@ -1,6 +1,18 @@
 
 class Candlestick {
-    constructor() {
-
+    constructor(
+        time,
+        open,
+        high,
+        low,
+        close,
+        volume
+    ) {
+        this.time = time;
+        this.open = open;
+        this.high = high;
+        this.low = low;
+        this.close = close;
+        this.volume = volume;
     }
 }
