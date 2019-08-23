6ef01cd52c251e37bd8cc9006a028ad867c5ce30
arya <aryad999dev@gmail.com>
2019-08-23 15:31:26 -0700

    kraken-client v1 of getAccountBalance

--- config/development.js
+++ config/development.js
@@ -2,7 +2,9 @@
 module.exports = {
     kraken: {
         api_key: process.env.kraken_api_key,
-        secret_api_key = process.env.kraken_secret_api_key
+        secret_api_key: process.env.kraken_secret_api_key,
+        api_url: process.env.kraken_api_url,
+        api_version: process.env.kraken_api_version
     },
     mysql: {
         host: process.env.mysql_host,
--- src/services/exchange/client/kraken-client.js
+++ src/services/exchange/client/kraken-client.js
@@ -1,20 +1,64 @@
+require('dotenv').config({ path: '../../../../.env' });
 const request = require('request');
 const crypto = require('crypto');
 const qs = require('qs');
 const KrakenConfig = require('../../../../config').kraken;
-
+const logger = require('../../../../utils/logger').getLogger();
 
 const kraken_api_key = KrakenConfig.api_key;
 const kraken_secret_api_key = KrakenConfig.secret_api_key;
+const kraken_api_url = KrakenConfig.api_url;
+const kraken_api_version = KrakenConfig.api_version;
 
+function createMessageSignature(path, postData, nonce) {
+	const message = qs.stringify(postData); //POST data as string
+	const secret_buffer = new Buffer.from(kraken_secret_api_key, 'base64');
+	const hash = new crypto.createHash('sha256');
+	const hmac = new crypto.createHmac('sha512', secret_buffer); // base64 decoded secret
+	const hash_digest = hash.update(nonce + message).digest('binary'); // SHA256(nonce + POST data)
+	const hmac_digest = hmac.update(path + hash_digest, 'binary').digest('base64'); //HMAC sha512 of results
 
-function createMessageSignature(postParams) {
-	const message       = qs.stringify(postParams);
-	const secret_buffer = new Buffer(kraken_secret_api_key, 'base64');
-	const hash          = new crypto.createHash('sha256');
-	const hmac          = new crypto.createHmac('sha512', secret_buffer);
-	const hash_digest   = hash.update(nonce + message).digest('binary');
-    const hmac_digest = hmac.update(path + hash_digest, 'binary').digest('base64');
-    
-    return hmac_digest;
+	return hmac_digest;
 }
+
+
+function getAccountBalance() {
+	let uri = '/' + kraken_api_version + '/private/TradeBalance';
+	let url = kraken_api_url + uri;
+
+	let nonce = (new Date() * 1000);
+	let postData = {nonce: nonce};
+
+	let headersJSON = {
+		'API-Key': kraken_api_key,
+		'API-Sign': createMessageSignature(uri, postData, nonce)
+	}
+
+	let options = {
+		url: url,
+		method: 'POST',
+		headers: headersJSON,
+		body: qs.stringify(postData)
+	}
+
+	return new Promise((resolve, reject) => {
+		request(options, (err, response, body) => {
+			if (err) {
+				console.log(`error: ${err}`)
+				reject(err);
+			}
+			else {
+				const returnresponse = JSON.parse(body);
+
+				resolve(returnresponse);
+
+			}
+		});
+	});
+
+}
+
+getAccountBalance()
+	.then(result => {
+		logger.info(result);
+	})
